ad048e9ea641c093c04968ffb06bc09168b69d15
Muni Sneha Sai Chembeti <91887869+SnehaChembeti@users.noreply.github.com>
2022-10-04 11:14:51 +0530

    Add files via upload

--- 04. Recursion/(09) Tower of hanoi.cpp
+++ 04. Recursion/(09) Tower of hanoi.cpp
@@ -9,7 +9,7 @@ void TOH (int n, char A, char B, char C)
         return;
     }
     TOH (n-1, A, C, B);
-    cout<<"move "<<n<<" from "<<A<<" to "<<C<<endl;
+    cout<<"move "<<n<<" from "<<A<<" to "<<C<<endl;      //O(2^n-1)
     TOH (n-1, B, A, C);
 }
 
